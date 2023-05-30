01dabb6a5506ab9a95122cc3166a31e9d4b07c12
Konstantin Shilov <shilovk@gmail.com>
2023-05-31 00:27:06 +0300

    feat(segment): create segment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+const Segment = ({ children, dashed, color }) => (
+  <div style={{ border: `1px ${dashed ? "dashed" : "solid" } ${color}` }}>
+    {/*{console.log(children) || children}*/}
+    {children.map(
+      (child, key) => (
+        <div key={key} style={{ border: "1px solid red" }}>
+          {typeof child == "function" ? child() : child}
+        </div>
+      )
+    )}
+  </div>
+);
+
+Segment.defaultProps = {
+  color: "black"
+}
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<div>Basic 2!</div>);
-
-
+root.render(
+  <Segment dashed>
+    <div>First</div>
+    <div>Second</div>
+    {() => <div>Third</div>}
+  </Segment>
+);
