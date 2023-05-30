8d7055ebeb09c118d863bf26ad82eca35ddc0f45
Konstantin Shilov <shilovk@gmail.com>
2023-05-30 23:47:10 +0300

    git(init): init app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<div>Basic 2!</div>);
+
+
